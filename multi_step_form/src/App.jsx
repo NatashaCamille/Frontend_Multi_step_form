61da44c0288a749b6e9e43508f6c1d426f1db292
NatashaCamille <nnalubega@outlook.com>
2023-08-22 23:13:26 +0300

    Updates

--- multi_step_form/src/App.jsx
+++ multi_step_form/src/App.jsx
@@ -5,6 +5,23 @@ import Step_3 from './components/Step_3';
 import Summary from './components/Summary';
 
 const App = () => {
+
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <Step1 formData={formData} onChange={handleFormChange} />;
+      case 2:
+        return <Step2 formData={formData} onChange={handleFormChange} />;
+      case 3:
+        return <Step3 formData={formData} onChange={handleFormChange} />;
+      case 4:
+        return <Summary formData={formData} />;
+      default:
+        return null;
+    }
+  };
+  
   return (
     <div className="App">
       <h1>Frontend Multi-Step Form</h1>
