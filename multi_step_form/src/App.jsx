dfbb0518c9eef0c4ac9caea7d7120bd73143d930
NatashaCamille <nnalubega@outlook.com>
2023-08-22 23:11:06 +0300

    Updates

--- multi_step_form/src/App.jsx
+++ multi_step_form/src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import Step_1 from './components/Step_1';
+import Step_2 from './components/Step_2';
+import Step_3 from './components/Step_3';
+import Summary from './components/Summary';
 
 const App = () => {
   return (
@@ -16,9 +19,8 @@ const App = () => {
           )}
         </div>
       </div>
-      <Step_1 />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
