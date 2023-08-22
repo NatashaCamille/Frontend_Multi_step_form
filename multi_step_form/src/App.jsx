fc91f876bf4d854ea23ce2122e37702e2ab88929
NatashaCamille <nnalubega@outlook.com>
2023-08-22 22:51:47 +0300

    Updates

--- multi_step_form/src/App.jsx
+++ multi_step_form/src/App.jsx
@@ -3,7 +3,19 @@ import Step_1 from './components/Step_1';
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
+      <h1>Frontend Multi-Step Form</h1>
+      <div className="form-container">
+        {renderStep()}
+        <div className="button-container">
+          {currentStep > 1 && (
+            <button onClick={handlePrevious}>Previous</button>
+          )}
+          {currentStep < 4 && (
+            <button onClick={handleNext}>Next</button>
+          )}
+        </div>
+      </div>
       <Step_1 />
     </div>
   )
