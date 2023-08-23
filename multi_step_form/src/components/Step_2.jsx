8e604274249b68d9529f269ab481af8803ff9c82
NatashaCamille <nnalubega@outlook.com>
2023-08-23 16:09:37 +0300

    Updates

--- multi_step_form/src/components/Step_2.jsx
+++ multi_step_form/src/components/Step_2.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Step_2 = () => {
   return (
-    <div>Step_2</div>
+    <div className="step">
+    <h2>Step 2</h2>
+    <input
+      type="text"
+      name="field2"
+      value={formData.field2}
+      onChange={handleFieldChange}
+      placeholder="Field 2"
+    />
+  </div>
   )
 }
 
